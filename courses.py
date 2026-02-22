0fb4dcf75156da559ec10b2ac9cb8e8aa028c2db
JacobKorin <jacob.korin@ontariotechu.net>
2026-02-22 16:35:51 -0500

    Route created for registering and de-registering for courses

--- courses.py
+++ courses.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
-from datetime import timedelta
+from datetime import timedelta, datetime,timezone
 from flask_jwt_extended import JWTManager, create_access_token, get_jwt, jwt_required, get_jwt_identity
 import os
 from dotenv import load_dotenv
@@ -18,23 +18,22 @@ uri="mongodb://localhost:27017/"
 client = MongoClient(uri)
 
 db = client.get_database("course_registration_portal")
+courses_collection = db.get_collection("courses")
+registration_collection = db.get_collection("registrations")
 
 @app.route("/courses", methods = ["GET"])
 def list_courses():
-    courses_collection = db.get_collection("courses")
-
     courses = list(courses_collection.find())
 
     for course in courses:
         course["_id"] = str(course["_id"])
         course["professor_id"] = str(course["professor_id"])
-        
+
     return jsonify(courses),200
 
 @app.route("/courses", methods = ["POST"])
 @jwt_required()
 def create_course():
-    courses_collection = db.get_collection("courses")
     role = get_jwt()["role"]
 
     if role == "professor":
@@ -48,5 +47,33 @@ def create_course():
     else:
         return jsonify({"error":"insufficient permissions"}),403
     
+@app.route("/courses/<course_id>/register", methods = ["POST"])
+@jwt_required()
+def course_register(course_id):
+    course_id = ObjectId(course_id)
+
+    if courses_collection.find_one({"_id":course_id}):
+        caller = ObjectId(get_jwt_identity())
+        timestamp = datetime.now(timezone.utc)
+        
+        registration = {"course_id":course_id,"student_id":caller,"registration_date":timestamp}
+        registration_collection.insert_one(registration)
+        
+        return jsonify({"status":"success"}),201
+    else:
+        return jsonify({"error":"course ID not found"}),404
+    
+@app.route("/courses/<course_id>/register", methods = ["DELETE"])
+@jwt_required()
+def course_deregister(course_id):
+    course_id = ObjectId(course_id)
+
+    if courses_collection.find_one({"_id":course_id}):
+        caller = ObjectId(get_jwt_identity())
+        registration_collection.delete_one({"course_id":course_id, "student_id":caller})
+
+        return jsonify({"status":"delete successful"}),200
+    else:
+        return jsonify({"error":"course ID not found"}),404
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5100)
